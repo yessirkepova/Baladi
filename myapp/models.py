c95d6ab9801d326b2bb706d3e7c73b6260f43486
AbilkaiyrMAXDEV <abilqaaiyr770@gmail.com>
2025-04-10 21:06:27 +0600

    2 try
    Але короче . написали фронт часть (не полностью ) админ част базу данных исправили . фронт част тепер внутриview писат будем. еще много чего но забыл че сделал

--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,16 @@ class Posts(models.Model):
   content= models.TextField(verbose_name='Контент')
   time_stamp=models.DateTimeField(default=timezone.now, verbose_name="время публикаций")
   edited=models.BooleanField(default=False,verbose_name="редактирован ли ?")
+
+
+def __str__(self):
+  return f'{self.title}:{self.time_stamp}'
+
+class PostAttachments(models.Model):
+  name=models.CharField(verbose_name='name of picture' ,blank=True, null=True)
+  image=models.ImageField(upload_to='files', verbose_name='FILEE')
+  post=models.ForeignKey(Posts,on_delete=models.CASCADE)
+
+  def save(self,*args,**kwargs):
+    self.name=self.image.name.split('.')[0].capitalize
+    super().save(*args,**kwargs)
